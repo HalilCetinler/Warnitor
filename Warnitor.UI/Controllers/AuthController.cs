080b52cc8e33a60086e5d75a6c342a81107b7b44
halil.cetinler <halil.cetinler@DESKTOP-VS6ACN8>
2023-11-06 14:39:51 +0300

    Add project files.

---
+++ Warnitor.UI/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Warnitor.UI.Models;
+
+namespace Warnitor.UI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+		public IActionResult AuthForgot()
+		{
+			return View();
+		}
+		public IActionResult Login()
+        {
+            return View();
+        }
+		public IActionResult AuthRegister()
+		{
+			return View();
+		}
+		
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
